e0b4f4217fd012867621c7dc527899d71bf8c579
Børge Hamre <borge.hamre@uib.no>
2022-08-04 10:54:57 +0200

    refactoring coating

--- coating/coating.hpp
+++ coating/coating.hpp
@@ -3,37 +3,41 @@
 
 #include "../environment/input_output.hpp"
 #include "../numeric/constants.hpp"
-#include "../polarization/fresnel.hpp"
-//#include "../polarization/isotropic_mueller.hpp"
+#include "../polarization/mueller.hpp"
+//#include "../polarization/fresnel.hpp"
+
 
 namespace flick {
 namespace coating {
-  class base
-  // Note that methods returning reflection and transmission angles
-  // are not necessarily giving correct energy distributions. They
-  // should rather be used as proposed angles and intensities should
-  // be scaled according to transmissivity and reflectivity.
-  {
+  
+  class angle_generator {
   protected:
-    double wavelength_{500e-9};
-    double rand_{1};
+    double ui_polar_{0};
+    double ui_azimuth_{0};
+  };
+
+  class lambert_angle_generator : public angle_generator {
   public:
-    void wavelength(double wl) {
-      wavelength_ = wl;
+    double polar_angle() {
+      return asin(sqrt(ui_polar_));
     }
-    void random_number(double rn) {
-      rand_ = rn;
+    double azimuth_angle() {
+      return ui_azimuth_ * 2 * constants::pi;
     }
+    double brdf() {
+      return 1/(2*constants::pi);
+    }
+  };
+ 
+  class base {
+  public:
     virtual mueller reflection_mueller_matrix()=0;
     virtual mueller transmission_mueller_matrix()=0;
     virtual double reflectivity()=0; 
     virtual double transmissivity()=0;
-    virtual double reflection_polar_angle()=0;
-    virtual double reflection_azimuth_angle()=0;
-    virtual double transmission_polar_angle()=0;
-    virtual double transmission_azimuth_angle()=0;
   };
-  
+
+  /*
   class isotropic : public base {
   protected:
     double incidence_angle_{0};
@@ -42,9 +46,18 @@ namespace coating {
       incidence_angle_ = ia;
     }
   };
+  class basic_lambert : public base {
 
-  class basic_lambert : public isotropic {
+  };
+  */
+  
+  class grey_lambert : public base {
+    double reflectivity_{1};
+    double transmissivity_{0};
   public:
+    grey_lambert(double reflectivity, double transmissivity)
+      : reflectivity_{reflectivity}, transmissivity_{transmissivity}
+    {}
     mueller reflection_mueller_matrix() {
       mueller m;
       m.add(0,0, 1/(2*constants::pi));
@@ -53,40 +66,39 @@ namespace coating {
     mueller transmission_mueller_matrix() {
       return reflection_mueller_matrix();
     }
-    double reflection_polar_angle() {
-      return asin(sqrt(rand_));
-    }
-    double reflection_azimuth_angle() {
-      return rand_ * 2 * constants::pi;
-    }
-    double transmission_polar_angle() {
-      return reflection_polar_angle();
-    }
-    double transmission_azimuth_angle() {
-      return reflection_azimuth_angle();
-    }
-  };
-  
-  class grey_lambert : public basic_lambert {
-    double reflectivity_{1};
-    double transmissivity_{0};
-  public:
-    grey_lambert(double reflectivity, double transmissivity)
-      : reflectivity_{reflectivity}, transmissivity_{transmissivity}
-    {}
     double reflectivity() {
-      return reflectivity_; 
+      return reflectivity_;
     }
     double transmissivity() {
       return transmissivity_;
     }
   };
-  
+
   class white_lambert : public grey_lambert {
   public:
     white_lambert() : grey_lambert::grey_lambert(1,0) {}
   };
 
+  class loamy_sand : base {
+    double wavelength_{500e-9};
+  public:
+    void wavelength(double wl) {
+      wavelength_ = wl;
+    }
+    double reflectivity() {
+      // wavelength dependent tbi
+      return 0;
+    }
+    double transmissivity() {
+      // wavelength dependent tbi
+      return 0;
+    }
+  };
+
+  
+ 
+  /*
+  // move to transport?
   class fresnel : public isotropic {
     std::complex<double> m_{1,0};
     flick::fresnel f_;
@@ -115,30 +127,9 @@ namespace coating {
     double transmissivity() {
       return f_.T();
     }
-    double reflection_polar_angle() {
-      return f_.reflection_angle();
-    }
-    double reflection_azimuth_angle() {
-      return 0;
-    }
-    double transmission_polar_angle() {
-      return std::real(f_.transmission_angle());
-    }
-    double transmission_azimuth_angle() {
-      return 0;
-    }
-    //private:
-    /*
-    double angular_distribution(double angle_0) {
-      double width = 1e-9;
-      double delta_mu = fabs(cos(angle_0)-cos(incidence_angle_));
-      std::cout << delta_mu;
-      if (delta_mu > width)
-	return 0;
-      return 1/(2*constants::pi*width);
-    }
-    */
   };
+  */
+
   /*
   class isotropic_composite : public isotropic {
     std::vector<std::shared_ptr<isotropic>> isotropics_;
@@ -146,6 +137,7 @@ namespace coating {
   public:
   };
   */
+
 }
 }
 
