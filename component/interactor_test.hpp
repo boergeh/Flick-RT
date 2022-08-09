d2ab5b28c4f87d6fff013357458f23db2e7888e6
Børge Hamre <borge.hamre@uib.no>
2022-08-09 09:47:29 +0200

    add missing files

---
+++ component/interactor_test.hpp
@@ -0,0 +1,11 @@
+#include "interactor.hpp"
+namespace flick {
+  begin_test_case(interactor_test) {
+    //lambert_surface_interactor lsi(1,0);
+    //pose p{{0,0,0},{1,0,0}};
+    //lsi.radiation_package(radiation_package{p,{1,0,0,0}});
+    //lsi.normal({1,0,0});
+    //std::cout << lsi.leaving_radiation().pose().z_direction();
+    //check(lsi.leaving_radiation().pose().z_direction().x() < 0);
+  } end_test_case()
+}
