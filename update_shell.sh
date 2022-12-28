c4e515d775f37ff3086aad260f95caed85c435ce
Børge Hamre <borge.hamre@uib.no>
2022-12-28 09:18:33 +0100

    even more automatic setting of environmental variables

--- update_shell.sh
+++ update_shell.sh
@@ -1,4 +1,4 @@
-export FLICK_FOLDER=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+FLICK_FOLDER=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 if [ -n "$($SHELL -c 'echo $ZSH_VERSION')" ]; then
     echo 'Need to add flick environmental variables to your ~/.zprofile'
     read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY]
@@ -6,10 +6,8 @@ if [ -n "$($SHELL -c 'echo $ZSH_VERSION')" ]; then
     echo 'export FLICK_PATH='$FLICK_FOLDER >> ~/.zprofile
     echo 'export FLICK_COMPILER="clang++ -std=c++20"' >> ~/.zprofile
     echo 'export PATH=$FLICK_PATH/main:$PATH' >> ~/.zprofile
-    #zsh
-    #source  ~/.zprofile
-    #$($SHELL -c '~/.zprofile')
-    
+    exec zsh -l
+    echo 'Three lines have been added to your .zprofile. Now, rerun make.'
 elif [ -n "$($SHELL -c 'echo $BASH_VERSION')" ]; then
     echo 'Need to add flick environmental variables to your ~/.profile'
     read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY]
@@ -17,11 +15,12 @@ elif [ -n "$($SHELL -c 'echo $BASH_VERSION')" ]; then
     echo 'export FLICK_PATH='$FLICK_FOLDER >> ~/.profile
     echo 'export FLICK_COMPILER="g++ -std=c++20"' >> ~/.profile
     echo 'export PATH=$FLICK_PATH/main:$PATH' >> ~/.profile
-    source  ~/.profile
+    exec bash -l
+    echo 'Three lines have been added to your .profile. Now, rerun make.'
+
 else
-    echo 'Unknown shell!'
-    echo 'Set these environmental variables in your shell profile'
-    echo 'and run make again'
+    echo 'Unknown shell. Set these environmental variables in your'
+    echo 'shell profile script, source it, and run make again'
     echo ''
     echo 'FLICK_PATH=$HOME/Flick-RT'
     echo 'FLICK_COMPILER="clang++ -std=c++20"'
