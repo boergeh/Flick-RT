41b3bbf2c5b1de32dd8aba36ee047f6efa7774fa
Børge Hamre <borge.hamre@uib.no>
2023-10-02 22:47:05 +0200

    add ocean material

---
+++ material/ocean.hpp
@@ -0,0 +1,35 @@
+#ifndef flick_material_ocean
+#define flick_material_ocean
+
+#include "water/pure_water.hpp"
+#include "mixture.hpp"
+#include "../environment/configuration.hpp"
+
+namespace flick {
+namespace material {
+  struct ocean : public mixture {
+    struct configuration : basic_configuration {
+      configuration() {
+	add<size_t>("angles", 2,
+		    "Number of computed phase function angles");
+	add<double>("bottom_depth", 200, "[m]");
+      }
+    };
+
+    ocean(const basic_configuration& c=ocean::configuration())
+      : mixture(range(0,constants::pi,c.get<size_t>("angles")).linspace(),
+		       {-c.get<double>("bottom_depth"), 0}) {
+      should_update_iops(false);
+      add_pure_water();     
+      should_update_iops(true);
+      update_iops();
+    }
+  private:
+    void add_pure_water() {
+      add_material<pure_water>();
+    }
+  };
+}
+}
+
+#endif
