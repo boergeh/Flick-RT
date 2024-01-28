a5539609e894874fd2b24d70c055431471b12da9
Børge Hamre <borge.hamre@uib.no>
2024-01-28 12:49:32 +0100

    simplified flick python module

--- Example/accurt_calls/logo/logo.py
+++ Example/accurt_calls/logo/logo.py
@@ -3,62 +3,45 @@ Make the Flick logo
 """
 import numpy as np
 import matplotlib.pyplot as plt
-import os
-import subprocess
 import sys
+sys.path.append('../ocean_radiance')
 from matplotlib import cm
 from matplotlib.colors import LightSource
-sys.path.append('../ocean_radiance')
 import flick
 
 recalculate = True
-black_background = False 
-
-f = ".tmp_flick_config"
-#if not os.path.exists(f):
-flick.run("accurt -g toa_reflectance "+f)
-        
-n_angles = 100
-n_polar = 50;
+black_background = False
+n_polar = 50
 n_azimuth = 150
-flick.config(f,"stream_upper_slab_size", flick.to_streams(n_angles))    
-flick.config(f,"detector_wavelengths", 300e-9)
-flick.config(f,"n_angles", n_angles)
-flick.config(f,"n_heights", 8)
-flick.config(f,"detector_height", -1)
-flick.config(f,"detector_orientation", "up")
-flick.config(f,"source_zenith_angle", 0)
-flick.config(f,"aerosol_od", 0)
-flick.config(f,"cloud_liquid", 0)
-flick.config(f,"nap_concentration", 0)
-flick.config(f,"chl_concentration", 0)
-flick.config(f,"cdom_440", 0)
-flick.config(f,"bottom_depth", 10000)
-flick.config(f,"detector_radiance_distribution_override",
-             np.array([n_polar, n_azimuth]))
+wl = 300e-9
+f = flick.radiance_distribution(wl,n_polar,n_azimuth)
+
+f.set_n_angles(100)
+f.set("n_heights", 8)
+f.set("detector_height", -1)
+f.set("source_zenith_angle", 0)
+f.set("aerosol_od", 0)
+f.set("cloud_liquid", 0)
+f.set("nap_concentration", 0)
+f.set("chl_concentration", 0)
+f.set("cdom_440", 0)
 
 if (recalculate):
     print("Using Flick to find UVR distributon at 1 m depth ...")
-    r = flick.run("accurt "+f)
+    r = f.values()
     np.save(".tmp_logo",r)
 else:
     r = np.load(".tmp_logo.npy")
 
-scaling_factor = 1.0    
-theta, phi = np.linspace(0, np.pi, n_polar),np.linspace(-np.pi, np.pi,
-                                                        n_azimuth)
-THETA, PHI = np.meshgrid(theta, phi)
-R = r.transpose()/r.max()*scaling_factor
-X = R * np.sin(THETA) * np.cos(PHI)
-Y = R * np.sin(THETA) * np.sin(PHI)
-Z = R * np.cos(THETA)
+r = r/r.max()    
+x,y,z = f.radiance_surface(r)
 
 if black_background:
     plt.style.use('dark_background')
 ls = LightSource(azdeg=0, altdeg=55)
-rgb = ls.shade(R, cmap=cm.hot, vert_exag=0, blend_mode='soft')
+rgb = ls.shade(r, cmap=cm.hot, vert_exag=0, blend_mode='soft')
 fig, ax = plt.subplots(subplot_kw=dict(projection='3d'))
-surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, facecolors=rgb,
+surf = ax.plot_surface(x, y, z, rstride=1, cstride=1, facecolors=rgb,
                        linewidth=0, antialiased=False, shade=False,alpha=1)
 
 font = {'fontname':'Arial'}
@@ -85,14 +68,3 @@ if black_background:
 else:
     fig.savefig("logo_white.png",dpi=500)
     fig.savefig("logo_white.jpg",dpi=500)
-
-
-
-
-
-
-
-
-
-
-
--- Example/accurt_calls/ocean_radiance/flick.py
+++ Example/accurt_calls/ocean_radiance/flick.py
@@ -8,6 +8,7 @@ example:
 """
 import numpy as np
 import subprocess
+import os
 
 def run(arguments):
     flick_output = _run_os("flick "+arguments)
@@ -37,7 +38,7 @@ def _to_matrix(flick_output):
         raise Exception(flick_output.stdout.decode('utf-8')) 
 
 def _to_string(numbers):
-    if isinstance(numbers, np.ndarray):
+    if isinstance(numbers, np.ndarray) or isinstance(numbers, list):
         return "\""+" ".join(str(x) for x in numbers)+"\""
     return str(numbers)
 
@@ -53,6 +54,141 @@ def to_streams(n_angles):
         n_streams += 1
     return str(n_streams).rstrip('0').rstrip('.');
 
+class basic_radiation:
+    _config_name = ".tmp_flick_config"
+    def _ensure_config_exists(self):
+        if not os.path.isfile(self._config_name):
+            run("accurt -g toa_reflectance "+self._config_name)
+            
+    def set(self, config_parameter, value):
+        self._ensure_config_exists()
+        config(self._config_name, config_parameter, value)
+        
+    def _relative_spectrum(self):
+        self._ensure_config_exists()
+        return run("accurt "+self._config_name)
+
+    def toa_zenith_irradiance(self,time_point_utc):
+        distance_au = run("sun_position distance "+time_point_utc)
+        r = run("radiator toa-solar")
+        r[:,1] = r[:,1] * (1/distance_au)**2
+        return r
+
+    def sun_zenith_angle(self,time_point_utc, latitude, longitude):
+        a = run("sun_position zenith_angle "+time_point_utc+" "+ \
+                               latitude+" "+longitude)
+        return a[0][0]
+        
+    def _absolute_spectrum(self, time_point_utc, latitude, longitude):
+        L_r = self._relative_spectrum()
+        F_0 = self.toa_zenith_irradiance(time_point_utc);
+        wl = F_0[:,0];
+        L_r = self._interpolate(L_r, wl)
+        a = self.sun_zenith_angle(time_point_utc, latitude, longitude)
+        s = L_r[:,1] * F_0[:,1] * np.cos(a*np.pi/180); 
+        return np.vstack((wl,s)).T
+
+    def _interpolate(self, points, x):       
+         x0 = points[:,0] 
+         y0 = points[:,1]
+         new_points = np.empty([len(x),2])
+         new_points[:,0] = x
+         new_points[:,1] = np.interp(x,x0,y0)
+         return new_points
+
+    def smooth(self, spectrum, from_wl, to_wl, width):
+        np.savetxt(".tmp_flick_spectrum", spectrum)
+        n_points = round(2*(to_wl - from_wl)/width)
+        wl = np.linspace(from_wl, to_wl, np)
+        spectrum = np.empty([len(wl),2])
+        spectrum[:,0] = wl
+        for i in range(len(wl)):
+            spectrum[i,1] = 0
+            run("filter .tmp_flick_spectrum gaussian_mean "+ \
+                      str(wl[i])+" "+str(width))
+        return spectrum
+
+    def set_n_angles(self,n_angles):
+         self.set("n_angles", round(n_angles)) 
+         self.set("stream_upper_slab_size", to_streams(round(n_angles))) 
+
+
+class radiance_distribution(basic_radiation):
+    def __init__(self, wl, n_polar, n_azimuth):
+        self.wl = wl
+        self.n_polar = n_polar
+        self.n_azimuth = n_azimuth
+        self.set("detector_wavelengths",self.wl)
+        self.set("detector_radiance_distribution_override",
+                 [self.n_polar, self.n_azimuth])
+        self.set_n_angles(16**1.6)
+        
+    def values(self):
+        return self._relative_spectrum().transpose()
+
+    def polar_angles(self):
+        return np.linspace(0, np.pi, self.n_polar)
+
+    def azimuth_angles(self):
+        return np.linspace(-np.pi, np.pi, self.n_azimuth)
+
+    def radiance_surface(self,r):
+        theta = self.polar_angles()
+        phi = self.azimuth_angles()
+        t, p = np.meshgrid(theta, phi)
+        x = r * np.sin(t) * np.cos(p)
+        y = r * np.sin(t) * np.sin(p)
+        z = r * np.cos(t)
+        return x,y,z
+    
+    
+class relative_radiation(basic_radiation):
+    def spectrum(self):
+        return self._relative_spectrum()
+
+
+class absolute_radiation(basic_radiation):
+    def spectrum(self):
+        return self._absolute_spectrum()
+
+    
+class radiance(absolute_radiation):
+    def __init__(self, polar_angle, azimuth_angle):
+        self.polar_angle = polar_angle
+        self.azimuth_angle = azimuth_angle
+        self.set_n_angles(16**1.6)
+        self.set("detector_orientation_override",[self.polar_angle,
+                                                  self.azimuth_angle])
+
+    def to_mW_per_m2_nm_sr(self,spectrum):
+        spectrum[:,0] = spectrum[:,0]*1e9
+        spectrum[:,1] = spectrum[:,1]*1e-6
+        return spectrum
+
+    
+class toa_radiance(radiance):
+    pass
+        
+
+class remote_sensing_reflectance(relative_radiation):
+    def __init__(self):
+        self.set("detector_height", 0.1)
+        self.set("subtract_specular_radiance", "true")
+        self.set_n_angles(16**1.6)
+
+        
+class surface_irradiance(absolute_radiation):
+    def __init__(self):
+        self.set("detector_height", 0.1)
+        self.set("detector_orientaion", "up")
+        self.set("detector_type", "irradiance")
+        self.set_n_angles(8**1.6)
+
+    def to_W_per_m2_nm(self,spectrum):
+        spectrum[:,0] = spectrum[:,0]*1e9
+        spectrum[:,1] = spectrum[:,1]*1e-3
+        return spectrum
+    
 
 
 
